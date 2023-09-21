d5670fca85e5dbbab29f51e5efb020a6be0c38a9
obb02 <1278025317@qq.com>
2023-09-21 15:45:04 +0800

    添加项目文件。

---
+++ Source/TencentClass/TencentClassGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TencentClassGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATencentClassGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATencentClassGameMode();
+};
+
+
+
